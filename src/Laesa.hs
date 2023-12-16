83224ff33c40648152ef4f63b96d3e02c5d8b447
Joshua Zhou <joshuazhou35@gmail.com>
2023-12-15 21:46:54 -0500

    First stab at predict

--- src/Laesa.hs
+++ src/Laesa.hs
@@ -1,22 +1,23 @@
-module Laesa () where
+module Laesa (MetricSpace, initLaesa, predict) where
 
-import Data.Array (Array, listArray, (!), (//))
-import Data.Foldable (toList)
+import Data.Array (listArray, (!))
 import Data.Ord (comparing)
-import Data.List (maximumBy)
+import Data.List (maximumBy, minimumBy)
+import qualified Data.Heap as Heap
 
 type DistMetric m = m -> m -> Double
 
 data MetricSpace m = MetricSpace{
-  mData :: Array Int m,
+  mData :: [m],  -- candidates
+  mDataCount :: Int,
   mDist :: DistMetric m
 }
 
 data Laesa m = Laesa{
   lMetricSpace :: MetricSpace m,
   lNumBases :: Int,
-  lBaseIndices :: Array Int Int,
-  lBaseDists :: Array (Int, Int) Double
+  lBaseIndices :: [Int],
+  lBaseDists :: [[Double]]  -- numBases x numCandidates
 }
 
 initLaesa :: MetricSpace m -> Int -> Laesa m
@@ -24,48 +25,52 @@ initLaesa ms numBases =
   Laesa{
     lMetricSpace = ms,
     lNumBases = numBases,
-    lBaseIndices = listArray (1, numBases) (map fst baseIndicesAndDists),
-    lBaseDists = listArray ((1, 1), (numBases, msDataCount)) (concatMap snd baseIndicesAndDists)
+    lBaseIndices = map fst baseIndicesAndDists,
+    lBaseDists = map snd baseIndicesAndDists
   }
   where msData = mData ms
-        msDataCount = length msData
+        msDataCount = mDataCount ms
         msDist = mDist ms
-        msDataList = toList msData
-        indexOfMax lst = fst $ maximumBy (comparing snd) (zip [0..] lst)
-        rawBaseIndicesAndDists = (0, [], replicate msDataCount 0) : 
-          [ (maxDistIndex, currBaseDists, lowerBounds) | 
+        msDataArr = listArray (1, msDataCount) msData
+        indexOfMax lst = fst $ maximumBy (comparing snd) (zip [1..] lst)
+
+        rawBaseIndicesAndDists = (1, [], replicate msDataCount 0) :
+          [ (maxDistIndex, currBaseDists, lowerBounds) |
             (baseIndex, _, prevLowerBounds) <- rawBaseIndicesAndDists,
-            let currBase = msData ! baseIndex, 
-            let currBaseDists = map (msDist currBase) msDataList, 
-            let lowerBounds = zipWith (+) currBaseDists prevLowerBounds, 
+            let currBase = msDataArr ! baseIndex,
+            let currBaseDists = map (msDist currBase) msData,
+            let lowerBounds = zipWith (+) currBaseDists prevLowerBounds,
             let maxDistIndex = indexOfMax lowerBounds ]
         baseIndicesAndDists = map (\(e1, e2, _) -> (e1, e2)) (tail rawBaseIndicesAndDists)
 
-{--
-foldUpdateSelect :: [m] -> DistMetric -> [[Double]] -> [Double]
+predict :: Laesa m -> m -> m
+predict la target = msDataArr ! i
+  where laMetricSpace = lMetricSpace la
+        -- laNumBases = lNumBases la
+        laBaseIndices = lBaseIndices la
+        laBaseDists = lBaseDists la
+        msData = mData laMetricSpace
+        msDataCount = mDataCount laMetricSpace
+        msDist = mDist laMetricSpace
+        msDataArr = listArray (1, msDataCount) msData
+        -- lBaseDistsArr = listArray ((1, 1), (laNumBases, msDataCount)) (concat laBaseDists)
+        absDiff a b = abs $ a - b
+        indexAndMin lst = minimumBy (comparing snd) (zip [1..] lst)
 
-
--- computeBase :: MetricSpace m -> Int -> Int -> [Double] ->
-computeBase ms baseRemain cand lowerBounds =
-  | baseRemain == 0 =
-  | otherwise =
-  where
-    ()
-    candidates = mData ms
-    foldUpdateSelect (oldMax, oldDist, oldBound) i =
-      | i in visited =
-      | otherwise = (max oldMax updatedBound, update i oldDist dist, update i oldBound updatedBound)
-      where
-        dist = (mDist ms) candidates[i] candidates[cand[-1]]
-        updatedBound =  oldBound[i] + dist
-
-buildLaesa :: MetricSpace m -> Int -> Laesa
-buildLaesa ms numBase = Laesa{
-  lMetricSpace=ms, lNumData=length (mData ms), lNumBase=numBase, lbase=base, lBaseDist=baseDict}
-  where base, baseDict = computeBase ms numBase
-
-nearestNeighbor :: Laesa m -> m -> m -- TODO custom untilM where we compare
-nearestNeighbor laesa taret =
-  where lowerBounds = computeLowerBounds
-
---}
+        targetDists = map (\ind -> msDist target (msDataArr ! ind)) laBaseIndices
+        -- targetDistsArr = listArray (1, laNumBases) targetDists
+        bounds = zipWith (map . absDiff) targetDists laBaseDists  -- (map . absDiff) == (\td bds -> map (absDiff td) bds)
+        lowerBounds = foldr (zipWith max) (replicate msDataCount 0) bounds
+        lowerBoundsArr = listArray (1, msDataCount) lowerBounds
+        candIndexWithBestDist lowerBoundHeap bestCandIndex bestDist
+          | Heap.null lowerBoundHeap = bestCandIndex
+          | otherwise =
+              let candIndex = Heap.payload (Heap.minimum lowerBoundHeap)
+              in if lowerBoundsArr ! candIndex > bestDist then bestCandIndex
+              else let newLowerBoundHeap = Heap.deleteMin lowerBoundHeap
+              in let newDist = msDist (msDataArr ! candIndex) target
+              in if newDist < bestDist then candIndexWithBestDist newLowerBoundHeap candIndex newDist
+              else candIndexWithBestDist newLowerBoundHeap bestCandIndex bestDist
+        (baseIndex, baseDist) = indexAndMin targetDists
+        baseCandIndex = laBaseIndices !! baseIndex
+        i = candIndexWithBestDist (Heap.fromList (zipWith Heap.Entry lowerBounds [1..])) baseCandIndex baseDist
